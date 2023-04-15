923fb16d025a00720d933f985c2376d0ed2d180b
NikoKamtsioras <ckamtsioras@gmail.com>
2023-04-15 15:36:41 +0300

    test1

--- test.py
+++ test.py
@@ -1,1 +1,12 @@
-import 
+import Adafruit_DHT as dht_sensor
+import time
+
+def get_temperature_readings():
+    humidity, temperature = dht_sensor.read_retry(dht_sensor.DHT22, 4)
+    humidity = format(humidity, ".2f") + "%"
+    temperature = format(temperature, ".2f") + "C"
+    return {"temperature": temperature, "humidity": humidity}
+
+while True:
+    print(get_temperature_readings())
+    time.sleep(30)
